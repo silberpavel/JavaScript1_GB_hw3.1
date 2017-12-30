1131bb64c50cccbfb525215c4d99c582e63653ea
Silber <silber.ypm@gmail.com>
2017-12-30 18:49:52 +0500

    3.2 completed, starting 3.3...

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 window.onload = function () {
 
-// Найти все элементы h1, являющиеся потомками div и заменить их содержимое на произвольное.
+    // 1. Найти все элементы h1, являющиеся потомками div и заменить их содержимое на произвольное.
     var allDivs = document.body.getElementsByTagName("div");
 
     for (let index = 0; index < allDivs.length; index++) {
@@ -11,5 +11,24 @@ window.onload = function () {
             divChilds[index].innerText = 'THIS MY NEW TEXT';
         }
     }
+
+    // 2. Установить всем input type="text" произвольное содержимое. Содержимое второго инпута должно отличаться от первого.
+    var inputs = document.body.getElementsByTagName("input");
+    for (let index = 0; index < inputs.length; index++) {
+        const element = inputs[index];
+        if (element.name == 'fname') {
+            element.value = 'Vasilis';
+        } else {
+            element.value = 'Vasilkov';
+        }
+    }
+
+    // 3. Выведите значение option с value=2
+
+
+
+
+
+
 
 }
